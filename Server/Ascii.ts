70db7317158902a52311a776885d80a9da53e495
errorcouldntloadname <thomas.birkenheuer@mailbox.tu-dresden.de>
2020-08-28 18:24:08 +0200

    customers can be served Kolle

--- Server/Ascii.ts
+++ Server/Ascii.ts
@@ -31,12 +31,14 @@ class Customer extends Schema {
 			this.hasPaid = true
 		}
 	}
+	
 }
 
 //state of the game in the current room
 class State extends Schema {
 	@type({ map: Player }) players = new MapSchema<Player>();
 	@type({ map: "number" }) Resources = new MapSchema<number>();
+	@type("number") score = 0;
 	//function to create a new player for given id  
 	createPlayer (id: string) { this.players[ id ] = new Player(); }
 	@type([Customer]) customers = new ArraySchema<Customer>();
@@ -87,16 +89,40 @@ export class Ascii extends Room {
 		this.state.players[client.sessionId].inventory = data;
 	}
 	
+	CustomerHappy(pos : number) {
+		let customer = this.state.customers[pos];
+		for (let desire in customer.wants) {
+			if (customer.wants[desire] != 0) { return; }
+		}
+		if (!customer.hasPaid) { return; }
+		this.state.customers.splice(pos,1);
+		this.state.score = this.state.score + 10;
+	}
+	
+	//serving stuff to customers
+	Serve (client : Client, pos : number) {
+		let inv = this.state.players[client.sessionId].inventory;
+		if (this.state.customers.length <= pos) { return; }
+		if (Object.keys(this.state.customers[pos].wants).lastIndexOf(inv) == -1) { return; } //MapSchema.has()
+		if (this.state.customers[pos].wants[inv] == 0) { return; }
+		this.state.customers[pos].wants[inv]--;
+		this.state.players[client.sessionId].inventory = "Empty";
+		this.CustomerHappy(pos);
+	}
 	//Message Handlers	
+	//delegates the "use" command to the action that is relevant for current position
 	onUse(client : Client, data : any) {
 		let player = this.state.players[client.sessionId];
-		if (player.x * 10 + player.y == 84 && player.rotation == 1)
+		if (player.x == 8 && player.y == 4 && player.rotation == 1)
 			{ this.Produce(client,"Kolle"); return; }
-		if (player.x * 10 + player.y == 85 && player.rotation == 1)
+		if (player.x == 8 && player.y == 5 && player.rotation == 1)
 			{ this.Produce(client,"Premium"); return; }
-		if (player.x * 10 + player.y == 86 && player.rotation == 1)
-			{ this.Produce(client,"Premium"); return; }
+		if (player.x == 8 && player.y == 6 && player.rotation == 1)
+			{ this.Produce(client,"Zotrine"); return; }
+		if (player.x == 2 && player.rotation == 3)
+			{ this.Serve(client,player.y); return; }
 	}
+	
 	onDrop (client: Client, data : any) {
 		if (this.state.players[client.sessionId].inventory == "Empty") { return; }
 		//TODO: do something usefull
@@ -145,6 +171,7 @@ export class Ascii extends Room {
 	
   onCreate (options: any) {
   	//read file
+  	//TODO: colyseus mapschema doc shows how ot loop through these wierd objects, makes code more variable
   	fs.createReadStream('Resources.csv')
   	.pipe(csv())
   	.on('data', (data : any) => {	//data contains a single line of the csv as "object" (behaves like struct)
@@ -183,7 +210,7 @@ export class Ascii extends Room {
   }
 
   generateCustomer(){
-  	if (this.state.customers.length >= 8) { return; }
+  	if (this.state.customers.length >= 7) { return; }
 	  var random : number = Math.floor(Math.random() * 10) // numbers between 0 and 10
 	  if(random > 7){
 	  	let wants = new MapSchema<number>();
