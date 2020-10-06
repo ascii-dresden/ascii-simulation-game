ede23607e0a9d61ca5d2f2f3567ce6f763ceee45
Anja Reusch <anja.reusch@posteo.org>
2020-10-06 11:21:26 +0200

    customers now show their order, order does not yet get delete when customer is happy

--- Defold/customer.go
+++ Defold/customer.go
@@ -33,3 +33,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "order_factory"
+  type: "factory"
+  data: "prototype: \"/order_item.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
