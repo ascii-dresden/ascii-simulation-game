e6a60efb4560134b1e84d5b8ca85fd5c604176b8
Anja Reusch <anja.reusch@posteo.org>
2020-10-18 15:28:49 +0200

    new sprites, speech bubble for customers

--- Defold/customer.go
+++ Defold/customer.go
@@ -51,3 +51,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "bubble"
+  type: "sprite"
+  data: "tile_set: \"/map.tilesource\"\n"
+  "default_animation: \"Speechbuble\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 23.0
+    y: 5.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
