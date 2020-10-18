00a68f63b4b556d5489958c03e9b1506242e7d2c
Anja Reusch <anja.reusch@posteo.org>
2020-10-18 22:41:33 +0200

    adjusted text in speech bubbles

--- Defold/order_item.go
+++ Defold/order_item.go
@@ -29,35 +29,35 @@ embedded_components {
   "  w: 0.0\n"
   "}\n"
   "color {\n"
-  "  x: 1.0\n"
-  "  y: 1.0\n"
-  "  z: 1.0\n"
+  "  x: 0.101960786\n"
+  "  y: 0.101960786\n"
+  "  z: 0.101960786\n"
   "  w: 1.0\n"
   "}\n"
   "outline {\n"
   "  x: 0.0\n"
   "  y: 0.0\n"
   "  z: 0.0\n"
-  "  w: 1.0\n"
+  "  w: 0.0\n"
   "}\n"
   "shadow {\n"
   "  x: 0.0\n"
   "  y: 0.0\n"
   "  z: 0.0\n"
-  "  w: 1.0\n"
+  "  w: 0.0\n"
   "}\n"
   "leading: 1.0\n"
   "tracking: 0.0\n"
-  "pivot: PIVOT_CENTER\n"
+  "pivot: PIVOT_W\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   "line_break: false\n"
   "text: \"\"\n"
-  "font: \"/builtins/fonts/system_font.font\"\n"
+  "font: \"/smaller.font\"\n"
   "material: \"/builtins/fonts/label.material\"\n"
   ""
   position {
-    x: 60.0
-    y: 20.0
+    x: 32.0
+    y: 50.0
     z: 0.1
   }
   rotation {
